df5e7f1a1a7b41160ee3da0b65fe12815318b9e9
Thomas Fan <thomasjpfan@gmail.com>
2019-05-08 20:25:17 -0400

    ENH Add helper in viewer

--- src/components/views/ModalWebViewer.js
+++ src/components/views/ModalWebViewer.js
@@ -1,36 +1,50 @@
-import React, { Component } from 'react'
-import { Modal, Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Modal, Button, Label } from "semantic-ui-react";
 
 class ModalWebViewer extends Component {
   state = {
     modalOpen: false,
     width: 0,
     height: 0
-  }
+  };
 
-  handleOpen = () => this.setState({ modalOpen: true })
-  handleClose = () => this.setState({ modalOpen: false })
+  handleOpen = () => this.setState({ modalOpen: true });
+  handleClose = () => this.setState({ modalOpen: false });
 
   render() {
+    const { innerHeight } = window;
+    const iframeHeight = innerHeight * 0.8;
 
-    const { innerHeight } = window
-    const iframeHeight = innerHeight * 0.8
-
-    return (<Modal
-      trigger={<Button onClick={this.handleOpen} {...this.props.buttonProps} />}
-      open={this.state.modalOpen}
-      onClose={this.handleClose}
-      size='fullscreen'
-      className='webmodal'
-    >
-      <Modal.Header>{this.props.title}</Modal.Header>
-      <Modal.Content>
-        <iframe title={this.props.title} src={this.props.iframeSrc} width="100%" height={`${iframeHeight}px`} ></iframe>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button negative onClick={this.handleClose}>Close</Button>
-      </Modal.Actions>
-    </Modal>)
+    return (
+      <Modal
+        trigger={
+          <Button onClick={this.handleOpen} {...this.props.buttonProps} />
+        }
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        size="fullscreen"
+        className="webmodal"
+      >
+        <Modal.Header>{this.props.title}</Modal.Header>
+        <Modal.Content>
+          <iframe
+            title={this.props.title}
+            src={this.props.iframeSrc}
+            width="100%"
+            height={`${iframeHeight}px`}
+            frameBorder={0}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Label size="large" color="green" className="dataset-helper-label">
+            Click tab above Figshare to View
+          </Label>
+          <Button negative onClick={this.handleClose}>
+            Close
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
   }
 }
 
